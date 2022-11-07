e856eb9330d6b61f0ad7231471a1aa98ae61c068
TheFolderOfAwesomeness <23sbaudler@castilleja.org>
2022-11-06 23:18:53 -0800

    horrible colors

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main extends PApplet{
     final int s = 600;
     final int x = s/2;
     final int y = s/2;
-    final int size = 10;
-    final int change = 20;
+    final int size = 5;
+    final int change = 15;
     public boolean coleur = true;
 
     public static void main(String[] args){
@@ -18,6 +18,7 @@ public class Main extends PApplet{
     }
 
     public void settings() {
+
         size(s, s);
     }
 
@@ -32,52 +33,52 @@ public class Main extends PApplet{
     public void fractal1(int xco, int yco, int siz, boolean color){
         Main.app.noStroke();
         if (color) {
-            Main.app.fill(random(255), 200, 200);
+            Main.app.fill(random(255), random(255), 200);
         } else {
             Main.app.fill (random(255), 0, random(255));
         }
         Main.app.circle(xco, yco, siz);
-        if (siz < 600){
-            fractal1(xco - change, yco - change, siz + change, !color);
+        if (siz < 1500){
+            fractal1(xco - change, yco - change, siz + (change*2), !color);
 
         }
     }
     public void fractal2(int xco, int yco, int siz, boolean color) {
         Main.app.noStroke();
         if (color) {
-            Main.app.fill(random(255), 200, 200);
+            Main.app.fill(random(255), random(255), 200);
         } else {
             Main.app.fill(random(255), 0, random(255));
         }
         Main.app.circle(xco, yco, siz);
-        if (siz < 600) {
-            fractal2(xco + change, yco - change, siz + change, !color);
+        if (siz < 1500) {
+            fractal2(xco + change, yco - change, siz + (change*2), !color);
         }
     }
 
     public void fractal3(int xco, int yco, int siz, boolean color) {
         Main.app.noStroke();
         if (color) {
-            Main.app.fill(random(255), 200, 200);
+            Main.app.fill(random(255), random(255), 200);
         } else {
             Main.app.fill(random(255), 0, random(255));
         }
         Main.app.circle(xco, yco, siz);
-        if (siz < 600) {
-            fractal3(xco + change, yco + change, siz + change, !color);
+        if (siz < 1500) {
+            fractal3(xco + change, yco + change, siz + (change*2), !color);
         }
     }
 
     public void fractal4(int xco, int yco, int siz, boolean color) {
         Main.app.noStroke();
         if (color) {
-            Main.app.fill(random(255), 200, 200);
+            Main.app.fill(random(255), random(255), 200);
         } else {
             Main.app.fill(random(255), 0, random(255));
         }
         Main.app.circle(xco, yco, siz);
-        if (siz < 600) {
-            fractal4(xco - change, yco + change, siz + change, !color);
+        if (siz < 1500) {
+            fractal4(xco - change, yco + change, siz + (change*2), !color);
         }
     }
 
